09cfb42f8f4c0549cc77fac1b8b185d8c7a35f6d
nsh1l <nsh1l@outlook.com>
2025-11-16 21:06:03 +0900

    親フォルダ名を変更

---
+++ src/_data/site.js
@@ -0,0 +1,9 @@
+module.exports = {
+  companyName: "有限会社 新田菓子舗",
+  phone: "0867-66-2116",
+  zipcode: "717-0505",
+  factory: "岡山県真庭市蒜山上長田459-4",
+  branch: "",
+  legacyTitle: "ようこそヒルゼンのお菓子屋　新田菓子舗へ",
+  urlGoogleMaps: "https://maps.app.goo.gl/tCymiqo1xKr2obK47",
+};
